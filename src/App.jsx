1448b059b3621dc756371b3c4233e38a6ca0429c
oto-webdev <r.otowebdev>
2024-10-20 19:07:59 +0400

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import State from './hooks/State';
 
 const App = () => {
   return (
     <div className=''> 
-      
+      <State />
     </div>
   )
 }
