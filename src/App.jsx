706d0760c1359672dd8fe9a8809437fec5946062
oto-webdev <r.otowebdev@gmail.com>
2024-12-02 01:04:07 -0800

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
